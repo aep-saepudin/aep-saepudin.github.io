9ce251c3a33c0b2f75e3c603ad7f2bd3b076d4ce
aep <aep.stmik@gmail.com>
2023-08-29 22:11:25 +0700

    update profile content

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // without js
+
+tippy('#myTippy', {
+    content: 'WHMCS Module, Payment Method Integration, Wordpress Custom Theme, Dashboard Internal, omg.id, app.comsys.id, daftarvaksinasi.com, Mobile App, Docker Setup, Deployment, auction system',
+    interactive: true,
+    trigger: "click",
+    allowHTML: true,
+  });
